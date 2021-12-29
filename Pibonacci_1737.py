d16cbc60365fd88a0091cf6787cf1786684a1d11
HalfMoonBeaR <mental0422@gmail.com>
2021-12-29 21:42:24 +0900

    git ""

---
+++ Pibonacci_1737.py
@@ -0,0 +1,15 @@
+from math import pi
+n = int(input())
+
+d = {}
+result = 0
+
+'''
+def pifibo(n):
+    if n <= pi and n >= 0:
+        return 1
+    return pifibo(n-1) + pifibo(n-pi)
+'''
+
+
+print(result)
