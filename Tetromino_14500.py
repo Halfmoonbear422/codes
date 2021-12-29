3855d91577871b35553656686b0bff1e747c377c
HalfMoonBeaR <mental0422@gmail.com>
2021-12-30 00:58:07 +0900

    Solving 14500

---
+++ Tetromino_14500.py
@@ -0,0 +1,14 @@
+'''
+Solving...
+https://www.acmicpc.net/problem/14500
+Is this worth solving?
+'''
+n ,m = map(int, input().split(()))
+board = []
+
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+
+
+max = 0
