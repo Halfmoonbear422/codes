b26d8c6d1fb9236df294a82ef97a0a22a2b70444
HalfMoonBeaR <mental0422@gmail.com>
2021-12-29 22:33:55 +0900

    Solved 1737

--- Pibonacci_1737.py
+++ Pibonacci_1737.py
@@ -1,16 +1,39 @@
+from collections import deque
 from math import pi
 n = int(input())
 
-d = {}
-result = 0
+d = {n:0}
+q = deque()
+q.append(n)
 
-'''
-def pifibo(n):
-    if n <= pi and n >= 0:
-        return 1
-    return pifibo(n-1) + pifibo(n-pi)
-    # no
-'''
+while q:
+    now = q.pop()
+    if now <= pi and now >=0:
+        d[now] = 1
+    else:
+        try:
+            n1 = d[now - 1]
+        except KeyError:
+            d[now-1] = 0
+            q.append(now)
+            q.append(now - 1)
+        try:
+            n2 = d[now - pi]
+        except KeyError:
+            d[now-pi] = 0
+            q.append(now)
+            q.append(now - pi)
+        try:
+            d[now] = n1 + n2
+        except NameError:
+            continue
 
+if n > pi:
+    result = d[n-1] + d[n-pi]
+else:
+    result = 1
 
-print(result)
+if result > 1e18:
+    print(result % int(1e18))
+else:
+    print(result)
